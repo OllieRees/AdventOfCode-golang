cd56fe81b0a3509095904f2e0f80d3f57e5c0fb8
Ollie Rees <oliver.x.rees@gmail.com>
2024-12-01 16:25:50 +0000

    UGLY GOLANG CODE

--- puzzles/historianhysteria.go
+++ puzzles/historianhysteria.go
@@ -1,7 +1,58 @@
 package puzzles
 
-import "iter"
+import (
+	"fmt"
+	"iter"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func locationLists(lines []string) (xs []int, ys []int) {
+	for _, line := range lines {
+		fields := strings.Fields(line)
+		x, _ := strconv.Atoi(fields[0])
+		y, _ := strconv.Atoi(fields[1])
+		xs = append(xs, x)
+		ys = append(ys, y)
+	}
+	return xs, ys
+}
+
+func seq2Array(seq iter.Seq[string]) (xs []string) {
+	for x := range seq {
+		xs = append(xs, x)
+	}
+	return xs
+}
+
+func frequencyCount(ys []int, x int) int {
+	count := 0
+	for _, y := range ys {
+		if y == x {
+			count++
+		}
+	}
+	return count
+}
 
 func HistorianHysteria(lines iter.Seq[string]) {
+	xs, ys := locationLists(seq2Array(lines))
+	diff_sum := 0
+	sim_score := 0
+	for _, x := range xs {
+		sim_score += x * frequencyCount(ys, x)
+	}
 
+	sort.Ints(xs); sort.Ints(ys)
+	for i := range xs {
+		diff := xs[i] - ys[i]
+		if diff < 0 {
+			diff = -1 * diff
+		}
+		diff_sum += diff
+	}
+
+	fmt.Println(diff_sum)
+	fmt.Println(sim_score)
 }
